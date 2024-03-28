9530c8df7a2c0c5edaf70a3b9311a4082edf8754
Rebekah Garris <rebekahlgarris@gmail.com>
2024-03-27 20:45:59 -0500

    adding methods to JwtsService to extract details from teken

--- OneThingApp/src/main/java/org/launchcode/OneThingApp/service/JwtService.java
+++ OneThingApp/src/main/java/org/launchcode/OneThingApp/service/JwtService.java
@@ -1,5 +1,6 @@
 package org.launchcode.OneThingApp.service;
 
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Service;
 
 import io.jsonwebtoken.Claims;
@@ -8,9 +9,11 @@ import io.jsonwebtoken.io.Decoders;
 import io.jsonwebtoken.security.Keys;
 
 import java.util.Date;
+import java.util.function.Function;
 
 import javax.crypto.SecretKey;
 
+import org.aspectj.lang.annotation.Before;
 import org.launchcode.OneThingApp.models.User;
 
 @Service
@@ -18,6 +21,28 @@ public class JwtService {
 	
 	private final String SECRET_KEY = "d0abf0b1c15f77d02c434f385b0a50c5c4d4dacad1db72070532dce1822bfad9";
 
+	public String extractUsername(String token) {
+		return extractClaim(token, Claims::getSubject);
+	}
+	
+	public boolean isValid(String token, UserDetails user) {
+		String usernameString = extractUsername(token);
+		return usernameString.equals(user.getUsername()) && !isTokenExpired(token);
+	}
+	
+	private boolean isTokenExpired(String token) {
+		return extractExpiration(token).before(new Date());
+	}
+
+	private Date extractExpiration(String token) {
+		return extractClaim(token, Claims::getExpiration);
+	}
+
+	public <T> T extractClaim(String token, Function<Claims, T> resolver) {
+		Claims claims = extractAllClaims(token);
+		return resolver.apply(claims);
+	}
+	
 	private Claims extractAllClaims(String token) {
 		return Jwts
 				.parser().verifyWith(getSigninKey())
