ef076bcb437317167f0dc3dd152aa759b6062160
Rebekah Garris <rebekahlgarris@gmail.com>
2024-04-01 16:07:55 -0500

    finished JwtAuthenticationFilter, I think...

--- OneThingApp/src/main/java/org/launchcode/OneThingApp/filter/JwtAuthenticationFilter.java
+++ OneThingApp/src/main/java/org/launchcode/OneThingApp/filter/JwtAuthenticationFilter.java
@@ -1,6 +1,75 @@
 package org.launchcode.OneThingApp.filter;
 
-public class JwtAuthenticationFilter {
+import java.io.IOException;
+
+import org.launchcode.OneThingApp.service.JwtService;
+import org.launchcode.OneThingApp.service.UserDetailsServiceImpl;
+import org.springframework.lang.NonNull;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import io.jsonwebtoken.security.Request;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+@Component
+public class JwtAuthenticationFilter extends OncePerRequestFilter{
+	//I'm extending the OncePerRequestFilter because I want this 
+	//filter to be executed once for every incoming request
+		
+	private final JwtService jwtService;
+	private final UserDetailsServiceImpl userDetailsService;
+	
+	public JwtAuthenticationFilter(JwtService jwtService, UserDetailsServiceImpl userDetailsService) {
+		this.jwtService = jwtService;
+		this.userDetailsService = userDetailsService;
+	}
+	
+	@Override
+	protected void doFilterInternal(
+			@NonNull HttpServletRequest request, 
+			@NonNull HttpServletResponse response, 
+			@NonNull FilterChain filterChain)
+			throws ServletException, IOException {
+		
+
+		String authHeader = request.getHeader("Authorization");
+		
+		if (authHeader == null || !authHeader.startsWith("Bearer ")) {
+			filterChain.doFilter(request, response);
+			return;
+		}
+		
+		String token = authHeader.substring(7);
+		String username = jwtService.extractUsername(token);
+		
+		if(username != null && SecurityContextHolder.getContext().getAuthentication() == null) {
+			
+			UserDetails userDetails = userDetailsService.loadUserByUsername(username);
+			
+			if(jwtService.isValid(token, userDetails)) {
+				UsernamePasswordAuthenticationToken authToken = 
+						new UsernamePasswordAuthenticationToken(
+								userDetails, null, userDetails.getAuthorities()
+				);
+				
+				authToken.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
+				
+				SecurityContextHolder.getContext().setAuthentication(authToken);
+			}
+			
+		}
+		
+		filterChain.doFilter(request, response);
+		
+	}
+	
 	
 	
 
