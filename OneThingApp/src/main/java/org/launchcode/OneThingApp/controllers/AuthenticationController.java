88c3ca9371d6f7fb9d6c7d11a6f44e816cf42b35
Rebekah Garris <rebekahlgarris@gmail.com>
2024-04-07 15:31:48 -0500

    coded constructor for AuthenticationController

--- OneThingApp/src/main/java/org/launchcode/OneThingApp/controllers/AuthenticationController.java
+++ OneThingApp/src/main/java/org/launchcode/OneThingApp/controllers/AuthenticationController.java
@@ -1,8 +1,19 @@
 package org.launchcode.OneThingApp.controllers;
 
-import org.springframework.stereotype.Controller;
+import org.launchcode.OneThingApp.service.AuthenticationService;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@RestController
 public class AuthenticationController {
+	
+	//this controller will handle login and registration requests
+	
+	private final AuthenticationService authenticationService;
+
+	public AuthenticationController(AuthenticationService authenticationService) {
+		this.authenticationService = authenticationService;
+	}
+	
+	
 
 }
