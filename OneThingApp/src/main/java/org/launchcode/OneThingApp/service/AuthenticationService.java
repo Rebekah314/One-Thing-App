d74c59bff1598d4a5e0262091bbdd2faf2cd85e6
Rebekah Garris <rebekahlgarris@gmail.com>
2024-04-07 14:57:29 -0500

    creating method for registering a new user

--- OneThingApp/src/main/java/org/launchcode/OneThingApp/service/AuthenticationService.java
+++ OneThingApp/src/main/java/org/launchcode/OneThingApp/service/AuthenticationService.java
@@ -1,8 +1,27 @@
 package org.launchcode.OneThingApp.service;
 
+import org.launchcode.OneThingApp.data.UserRepository;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
 public class AuthenticationService {
+	
+	private final UserRepository userRepository;
+	private final PasswordEncoder passwordEncoder;
+	private final JwtService jwtService;
+	
+	public AuthenticationService(UserRepository userRepository, PasswordEncoder passwordEncoder,
+			JwtService jwtService) {
+		this.userRepository = userRepository;
+		this.passwordEncoder = passwordEncoder;
+		this.jwtService = jwtService;
+	}
+	
+	//create method for registration
+	
+	public AuthenticationResponse register(RegistrationRequest request) {
+		
+	}
 
 }
