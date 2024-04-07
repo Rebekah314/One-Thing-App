d424b355013b1d9b0547297d825b739278a7f8a1
Rebekah Garris <rebekahlgarris@gmail.com>
2024-04-07 15:15:51 -0500

    coded authenticationResponse class to hold and retrieve token

---
+++ OneThingApp/src/main/java/org/launchcode/OneThingApp/models/AuthenticationResponse.java
@@ -0,0 +1,17 @@
+package org.launchcode.OneThingApp.models;
+
+public class AuthenticationResponse {
+	
+	private String token;
+
+	public AuthenticationResponse(String token) {
+		this.token = token;
+	}
+
+	public String getToken() {
+		return token;
+	}
+
+	
+
+}
