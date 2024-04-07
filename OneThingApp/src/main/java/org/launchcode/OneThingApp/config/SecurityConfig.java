53efc3633c856a268d5e0f9ae12aff35c33bf326
Rebekah Garris <rebekahlgarris@gmail.com>
2024-04-07 14:48:12 -0500

    add authentication manager to security config class

--- OneThingApp/src/main/java/org/launchcode/OneThingApp/config/SecurityConfig.java
+++ OneThingApp/src/main/java/org/launchcode/OneThingApp/config/SecurityConfig.java
@@ -1,9 +1,12 @@
 package org.launchcode.OneThingApp.config;
 
+
 import org.launchcode.OneThingApp.filter.JwtAuthenticationFilter;
 import org.launchcode.OneThingApp.service.UserDetailsServiceImpl;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
@@ -12,7 +15,7 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
-import org.springframework.security.web.header.writers.frameoptions.RegExpAllowFromStrategy;
+
 
 @Configuration
 @EnableWebSecurity
@@ -50,6 +53,11 @@ public class SecurityConfig {
 	@Bean
 	public PasswordEncoder passwordEncoder() {
 		return new BCryptPasswordEncoder();
+	}
+	
+	@Bean 
+	public AuthenticationManager authenticationManager(AuthenticationConfiguration configuration) throws Exception {
+		return configuration.getAuthenticationManager();
 	}
 
 }
