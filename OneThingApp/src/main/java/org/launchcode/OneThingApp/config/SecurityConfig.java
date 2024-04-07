23a84cd35e4b3124b92883da5b2f28b874996a95
Rebekah Garris <rebekahlgarris@gmail.com>
2024-04-07 14:39:33 -0500

    coding security filter chain

--- OneThingApp/src/main/java/org/launchcode/OneThingApp/config/SecurityConfig.java
+++ OneThingApp/src/main/java/org/launchcode/OneThingApp/config/SecurityConfig.java
@@ -1,5 +1,48 @@
 package org.launchcode.OneThingApp.config;
 
+import org.launchcode.OneThingApp.filter.JwtAuthenticationFilter;
+import org.launchcode.OneThingApp.service.UserDetailsServiceImpl;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.security.web.header.writers.frameoptions.RegExpAllowFromStrategy;
+
+@Configuration
+@EnableWebSecurity
 public class SecurityConfig {
+	
+	private final UserDetailsServiceImpl userDetailsServiceImpl;
+	
+	private final JwtAuthenticationFilter jwtAuthenticationFilter;
+	
+	public SecurityConfig(UserDetailsServiceImpl userDetailsServiceImpl,
+			JwtAuthenticationFilter jwtAuthenticationFilter) {
+		this.userDetailsServiceImpl = userDetailsServiceImpl;
+		this.jwtAuthenticationFilter = jwtAuthenticationFilter;
+	}
+
+
+
+	@Bean
+	public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+		
+		return http
+				.csrf(AbstractHttpConfigurer::disable)
+				.authorizeHttpRequests(
+						req->req.requestMatchers("/login/**", "/register/**")
+						.permitAll()
+						.anyRequest()
+						.authenticated()
+				).userDetailsService(userDetailsServiceImpl)
+				.sessionManagement(session->session
+						.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+				.addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class)
+				.build();
+	}
 
 }
