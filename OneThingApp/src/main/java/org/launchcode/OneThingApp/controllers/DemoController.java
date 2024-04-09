551f0f096c93866c86696271d56b515b4eb0c8e6
Rebekah Garris <rebekahlgarris@gmail.com>
2024-04-09 13:47:49 -0500

    adding demo landing page to DemoController

--- OneThingApp/src/main/java/org/launchcode/OneThingApp/controllers/DemoController.java
+++ OneThingApp/src/main/java/org/launchcode/OneThingApp/controllers/DemoController.java
@@ -1,5 +1,15 @@
 package org.launchcode.OneThingApp.controllers;
 
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class DemoController {
+	
+	@GetMapping("/demo")
+	public ResponseEntity<String> demo(){
+		return ResponseEntity.ok("Hello from secured url");
+	}
 
 }
