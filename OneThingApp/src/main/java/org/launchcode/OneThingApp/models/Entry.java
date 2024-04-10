645b644cd1f1e5ed7d063a5deb14b7beb7ff6f16
Rebekah Garris <rebekahlgarris@gmail.com>
2024-04-09 21:15:52 -0500

    basic setup of Entry is done. TODOs for future

--- OneThingApp/src/main/java/org/launchcode/OneThingApp/models/Entry.java
+++ OneThingApp/src/main/java/org/launchcode/OneThingApp/models/Entry.java
@@ -1,5 +1,31 @@
 package org.launchcode.OneThingApp.models;
 
+import java.time.LocalDate;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
 public class Entry {
+	
+	@Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+	
+	private LocalDate date;
+	
+	private String content;
+	
+	@Enumerated(value = EnumType.STRING)
+    private Status status;
+	
+	//TODO link to author
+	//TODO link to accountability partner "friend"
+	
+	//TODO tag with another enum: personal, family, home, financial, car, errand, other 
 
 }
