db2ddd31de764ac6b8133d0d7f78304a9f1c2b07
Rebekah Garris <rebekahlgarris@gmail.com>
2024-04-17 11:48:59 -0500

    removed extraneous homepage stuff. tested infinite loop with setEffect

--- OneThingApp/front-end/src/App.js
+++ OneThingApp/front-end/src/App.js
@@ -45,18 +45,7 @@ useEffect(() => {
       <h1>Hello World!!!  </h1>
       <h2>JWT value is {jwt}</h2>
 
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+
       </header>
     </div>
   );
