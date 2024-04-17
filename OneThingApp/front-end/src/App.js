b0ac45e54c03fbb291f57cfeace5d9122919db0a
Rebekah Garris <rebekahlgarris@gmail.com>
2024-04-17 10:42:31 -0500

    adding commentary for readability

--- OneThingApp/front-end/src/App.js
+++ OneThingApp/front-end/src/App.js
@@ -8,6 +8,7 @@ console.log("Hello!");
 
 //**TO DO** figure out why code is running twice
 //try "use effect" hook
+//empty dependency array (second input) tells app to run this code once upon load
 
 useEffect(() => {
   const reqBody = {
