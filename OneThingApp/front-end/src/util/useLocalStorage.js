4f700c77687cb948137863df4e1ea16d404cf879
Rebekah Garris <rebekahlgarris@gmail.com>
2024-04-18 09:28:48 -0500

    persisting react state in local storage

--- OneThingApp/front-end/src/util/useLocalStorage.js
+++ OneThingApp/front-end/src/util/useLocalStorage.js
@@ -1,8 +1,27 @@
+//following this video from coderCampus: 
+//https://www.youtube.com/watch?v=aIr288-3AFE&list=PL2OrQJM8zmZ2-O_rM2Ju9zYMbY8Ta-8I4&index=12
+
+//which is following this page:
+//https://www.joshwcomeau.com/react/persisting-react-state-in-localstorage/
+
 function useLocalState (defaultValue, key) {
 
     //local storage stores strings
     //if object needs to be stored (e.g., json), must stringify first
 
+    const [value, setValue] = React.useState(() => {
+        const localState = window.localStorage.getItem(key);
+
+        return localState !== null
+        ? JSON.parse(localState)
+        : defaultValue;
+    });
+
+    React.useEffect(() => {
+        window.localStorage.setItem(key, JSON.stringify(value));
+    }, [key, value]);
+
+    return [value, setValue];
 }
 
 export {useLocalState};
