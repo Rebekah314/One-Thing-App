534b62190b1f6dd0c9a1ae2aa645c9f6eb48ac24
Rebekah Garris <rebekahlgarris@gmail.com>
2024-04-18 09:20:18 -0500

    adding helpful commentary

--- OneThingApp/front-end/src/util/useLocalStorage.js
+++ OneThingApp/front-end/src/util/useLocalStorage.js
@@ -1,3 +1,8 @@
 function useLocalState (defaultValue, key) {
-    
+
+    //local storage stores strings
+    //if object needs to be stored (e.g., json), must stringify first
+
 }
+
+export {useLocalState};
