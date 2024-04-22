112f134181de29fb3d825b1f9588307c5f2336fe
Rebekah Garris <rebekahlgarris@gmail.com>
2024-04-22 10:32:55 -0500

    working on entry creation from backend

---
+++ OneThingApp/src/main/java/org/launchcode/OneThingApp/controllers/EntryController.java
@@ -0,0 +1,24 @@
+package org.launchcode.OneThingApp.controllers;
+
+import org.launchcode.OneThingApp.models.Entry;
+import org.launchcode.OneThingApp.models.User;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/entries")
+public class EntryController {
+	
+	EntryService entryService;
+	
+	@PostMapping("")
+	public ResponseEntity<Entry> createEntry(@AuthenticationPrincipal User user){
+		
+		//want access to user information for user logged in
+		
+	}
+
+}
