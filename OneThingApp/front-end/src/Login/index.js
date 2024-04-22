d5f7c167a25f8e1639f82231c8cf9dbfec0513f7
Rebekah Garris <rebekahlgarris@gmail.com>
2024-04-21 20:28:08 -0500

    creating and coding Login component

---
+++ OneThingApp/front-end/src/Login/index.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+const Login = () => {
+    return (
+        <>
+            <div>
+                <label htmlFor="username">Username</label>
+                    <input type="text" id="username" />
+            </div>
+            <div>
+                <label htmlFor="password">Password</label>
+                    <input type="password" id="password" />
+            </div>
+        </>
+    );
+};
+
+export default Login;
