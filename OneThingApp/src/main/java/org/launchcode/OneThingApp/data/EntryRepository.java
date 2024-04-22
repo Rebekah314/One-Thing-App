ae424b2fb5a30bad733dd47716763077f988fb23
Rebekah Garris <rebekahlgarris@gmail.com>
2024-04-22 10:50:32 -0500

    changing id type to long in both repositories

--- OneThingApp/src/main/java/org/launchcode/OneThingApp/data/EntryRepository.java
+++ OneThingApp/src/main/java/org/launchcode/OneThingApp/data/EntryRepository.java
@@ -3,6 +3,6 @@ package org.launchcode.OneThingApp.data;
 import org.launchcode.OneThingApp.models.Entry;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface EntryRepository extends JpaRepository<Entry, Integer>{
+public interface EntryRepository extends JpaRepository<Entry, Long>{
 
 }
