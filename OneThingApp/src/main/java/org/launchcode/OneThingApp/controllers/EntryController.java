168e738953a085588379b2219857d1dfeeabb02f
Rebekah Garris <rebekahlgarris@gmail.com>
2024-04-23 09:50:05 -0500

    updating EntryController method to getEntries by user

--- OneThingApp/src/main/java/org/launchcode/OneThingApp/controllers/EntryController.java
+++ OneThingApp/src/main/java/org/launchcode/OneThingApp/controllers/EntryController.java
@@ -1,10 +1,13 @@
 package org.launchcode.OneThingApp.controllers;
 
+import java.util.Set;
+
 import org.launchcode.OneThingApp.models.Entry;
 import org.launchcode.OneThingApp.models.User;
 import org.launchcode.OneThingApp.service.EntryService;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -30,5 +33,12 @@ public class EntryController {
 		return ResponseEntity.ok(newEntry);
 		
 	}
+	
+	@GetMapping("")
+	public ResponseEntity<?> getEntries(@AuthenticationPrincipal User user){
+		Set<Entry> entriesByUser = entryService.findByUser(user);
+		return ResponseEntity.ok(entriesByUser);
+	}
+	
 
 }
