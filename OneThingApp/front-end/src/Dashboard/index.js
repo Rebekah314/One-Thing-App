4fddc6b6451e993fd149111248e381ef80fed712
Rebekah Garris <rebekahlgarris@gmail.com>
2024-04-23 09:28:03 -0500

    fetching entries to display on dashboard

--- OneThingApp/front-end/src/Dashboard/index.js
+++ OneThingApp/front-end/src/Dashboard/index.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { useLocalState } from '../util/useLocalStorage';
 
 const Dashboard = () => {
@@ -7,6 +7,18 @@ const Dashboard = () => {
 
     //need a variable to store all the data, 
     //so that it can be displayed in the view below
+    const [entries, setEntries] = useState("null");
+
+    useEffect(() => {
+        fetch("entries", {
+            headers: {
+                "Content-Type": "application/json",
+                "Authorization": `Bearer ${jwt}`
+            },
+            method: "GET",
+        })
+    }, []);
+
 
     function createEntry() {
         fetch("/entries", {
@@ -27,7 +39,7 @@ const Dashboard = () => {
     return (
         <div style={{margin: "2em"}}>
             <h1>Welcome back!</h1>
-            <h3>What's the ONE thing I can do today that will make everythign else easier or unnecessary?</h3>
+            <h3>What's the ONE thing I can do today that will make everything else easier or unnecessary?</h3>
 
 
             <textarea id="oneThing" name="oneThing" rows="4" cols="50">
@@ -36,6 +48,9 @@ const Dashboard = () => {
 
 
             <button onClick={() => createEntry()}>Save Entry: my ONE thing today</button>
+            <hr />
+            <h3>Priority Prism</h3>
+            {entries ? entries.map(entry => <div></div>) : <></>}
             <hr />
             <p> JWT Value is {jwt}</p>
         </div>
