c7d3fae27f0d111fed252aafd0c926a882aa7f3b
Rebekah Garris <rebekahlgarris@gmail.com>
2024-04-23 09:42:54 -0500

    adding method to EntryRepository to find entries by user

--- OneThingApp/src/main/java/org/launchcode/OneThingApp/data/EntryRepository.java
+++ OneThingApp/src/main/java/org/launchcode/OneThingApp/data/EntryRepository.java
@@ -1,8 +1,13 @@
 package org.launchcode.OneThingApp.data;
 
+import java.util.Set;
+
 import org.launchcode.OneThingApp.models.Entry;
+import org.launchcode.OneThingApp.models.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface EntryRepository extends JpaRepository<Entry, Long>{
+	
+	Set<Entry> findByUser(User user);
 
 }
