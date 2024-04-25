1a96f5e57150c2a645f913b2fd28b330ecbfa333
Rebekah Garris <rebekahlgarris@gmail.com>
2024-04-25 09:59:19 -0500

    need to retrieve entry fields based on id

--- OneThingApp/front-end/src/EntryView/index.js
+++ OneThingApp/front-end/src/EntryView/index.js
@@ -1,9 +1,12 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const EntryView = () => {
 
 //TO DO: get access to id in a better way. For now, pull from url
 const entryId = window.location.href.split("/entries/")[1];
+
+//retrieve entry by ID
+const [entry, setEntry] = useState(null);
 
 //TO DO: entry fields should only be able to be updated 
 //by the author, and maybe the accountability friend
@@ -11,6 +14,7 @@ const entryId = window.location.href.split("/entries/")[1];
     return (
         <div>
             <h1>Entry {entryId}</h1>
+            <h2>Status: {entry.status}</h2>
 
         </div>
     );
