f23111bda3f8662c5ac643baee28474da8a88446
Rebekah Garris <rebekahlgarris@gmail.com>
2024-04-25 09:50:55 -0500

    displaying Entry ID tite on individual entry page for now

--- OneThingApp/front-end/src/App.js
+++ OneThingApp/front-end/src/App.js
@@ -6,6 +6,7 @@ import Dashboard from './Dashboard';
 import Homepage from './Homepage';
 import Login from './Login';
 import PrivateRoute from './PrivateRoute';
+import EntryView from './EntryView';
 
 function App() {
 
--- OneThingApp/front-end/src/EntryView/index.js
+++ OneThingApp/front-end/src/EntryView/index.js
@@ -1,9 +1,13 @@
 import React from 'react';
 
 const EntryView = () => {
+
+//TODO get access to id in a better way. For now, pull from url
+const entryId = window.location.href.split("/entries/")[1];
+
     return (
         <div>
-            
+            <h1>Entry {entryId}</h1>
         </div>
     );
 };
