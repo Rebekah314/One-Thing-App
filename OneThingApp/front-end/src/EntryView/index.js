28d1cf5ef677da558dffcb7e1a126a573c2ab071
Rebekah Garris <rebekahlgarris@gmail.com>
2024-04-26 14:52:36 -0500

    setting up Entry view page

--- OneThingApp/front-end/src/EntryView/index.js
+++ OneThingApp/front-end/src/EntryView/index.js
@@ -8,12 +8,23 @@ const entryId = window.location.href.split("/entries/")[1];
 //retrieve entry by ID
 const [entry, setEntry] = useState(null);
 
+useEffect(() => {
+    fetch()
+}, []);
+
 //TO DO: entry fields should only be able to be updated 
 //by the author, and maybe the accountability friend
 
     return (
         <div>
             <h1>Entry {entryId}</h1>
+            {entry ? (
+            <>
+
+            </>
+            ) : (
+            <></>
+            )}
             <h2>Status: {entry.status}</h2>
 
         </div>
