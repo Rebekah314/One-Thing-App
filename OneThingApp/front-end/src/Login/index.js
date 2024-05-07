54e019f1d07ddcd532cac3985f35aa776cdc550c
Rebekah Garris <rebekahlgarris@gmail.com>
2024-05-07 10:22:38 -0500

    beginning to include bootstrap for FE design

--- OneThingApp/front-end/src/Login/index.js
+++ OneThingApp/front-end/src/Login/index.js
@@ -3,14 +3,16 @@ import React from 'react';
 const Login = () => {
     return (
         <>
-            <div>
-                <label htmlFor="username">Username</label>
-                    <input type="text" id="username" />
-            </div>
-            <div>
-                <label htmlFor="password">Password</label>
-                    <input type="password" id="password" />
-            </div>
+            <Container>
+                <div>
+                    <label htmlFor="username">Username</label>
+                        <input type="text" id="username" />
+                </div>
+                <div>
+                    <label htmlFor="password">Password</label>
+                        <input type="password" id="password" />
+                </div>
+            </Container>
         </>
     );
 };
