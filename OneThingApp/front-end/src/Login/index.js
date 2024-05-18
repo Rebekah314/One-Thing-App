2df596065e2eb6220afc432d4fc533a161a49179
Rebekah Garris <rebekahlgarris@gmail.com>
2024-05-18 07:23:05 -0500

    working on login page

--- OneThingApp/front-end/src/Login/index.js
+++ OneThingApp/front-end/src/Login/index.js
@@ -1,16 +1,37 @@
 import React from 'react';
 
 const Login = () => {
+
+
+    
     return (
         <>
             <Container>
                 <div>
                     <label htmlFor="username">Username</label>
-                        <input type="text" id="username" />
+                        <input 
+                            type="text" 
+                            id="username"
+                            value={username}
+                            onChange={(event) => setUsername(event.target.value)} 
+                        />
                 </div>
                 <div>
                     <label htmlFor="password">Password</label>
-                        <input type="password" id="password" />
+                        <input 
+                            type="password" 
+                            id="password"
+                            value={password}
+                            onChange={(event) => setPassword(event.target.value)} 
+                        />
+                </div>
+                <div>
+                    <button 
+                        id="submit" 
+                        tyep="button"
+                        onClick={() => sendLoginRequest()}>
+
+                    </button>
                 </div>
             </Container>
         </>
