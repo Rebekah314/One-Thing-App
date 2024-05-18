8b1e12704103788d8dfd7e51f0380aa2914b76c4
Rebekah Garris <rebekahlgarris@gmail.com>
2024-05-18 09:12:35 -0500

    ran into bug, so deleted a few steps and redoing.

--- OneThingApp/front-end/src/Login/index.js
+++ OneThingApp/front-end/src/Login/index.js
@@ -1,25 +1,12 @@
-import React, { useState } from 'react';
-import { reusableFetch } from '../Services/reusableFetch';
-import { useLocalState } from '../util/useLocalStorage';
+import React from 'react';
+
 
 const Login = () => {
 
-    const [username, setUsername] = useState("");
-    const [password, setPassword] = useState("");
-    const [jwt, setJwt] = useLocalState("", "jwt");
-
-    const loginBody = {
-        "username": {username},
-        "password": {password}
-    }
-
     function sendLoginRequest() {
-        reusableFetch("/login", "POST", null, loginBody).then((response) => {
-            setJwt(response);
-            return window.location.href = "/dashboard";
-        });
-        
+        console.log("I'm logging in");
     }
+
 
     return (
         <>
@@ -29,8 +16,7 @@ const Login = () => {
                         <input 
                             type="text" 
                             id="username"
-                            value={username}
-                            onChange={(event) => setUsername(event.target.value)} 
+ 
                         />
                 </div>
                 <div>
@@ -38,18 +24,15 @@ const Login = () => {
                         <input 
                             type="password" 
                             id="password"
-                            value={password}
-                            onChange={(event) => setPassword(event.target.value)} 
+
                         />
                 </div>
                 <div>
-                    <button 
-                        id="submit" 
-                        tyep="button"
-                        onClick={() => sendLoginRequest()}>
-
+                    <button id="submit" type="button" onClick={() => sendLoginRequest()}>
+                        Log In
                     </button>
                 </div>
+
             
         </>
     );
