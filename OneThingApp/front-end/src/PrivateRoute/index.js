1e4367168c60c5b65840ac36a6bbb6c49f4bc4cf
Rebekah Garris <rebekahlgarris@gmail.com>
2024-05-20 11:46:13 -0500

    working on frontend code for jwt validation

--- OneThingApp/front-end/src/PrivateRoute/index.js
+++ OneThingApp/front-end/src/PrivateRoute/index.js
@@ -1,16 +1,25 @@
 import React from 'react';
 import { Navigate } from 'react-router-dom';
 import { useLocalState } from '../util/useLocalStorage';
+import { reusableFetch } from '../Services/reusableFetch';
 
 //If the user is authenticated, we want to render the "children".
 //If the user is not authentication, we want to redirect to the Login page.
 
 //This needs to check if the jwt is expired, not just if it's empty
+//JwtService method 'isValid' checks if username matches database and if token is expired
 
 const PrivateRoute = ( { children }) => {
 
     //check if there is a stored jwt value
     const [jwt, setJwt] = useLocalState("", "jwt");
+
+    if (jwt) {
+        //don't pass in jwt because we don't want it to reject it automatically
+        reusableFetch(`/validate?token=${jwt}`, "GET", null ).then();
+    }
+
+    
 
     return jwt ? children : <Navigate to="/login" />;
 };
