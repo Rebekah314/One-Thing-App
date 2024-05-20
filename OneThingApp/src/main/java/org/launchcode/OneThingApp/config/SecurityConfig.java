6e331c848732c51f0acd826cd9d169a5b26b2f4e
Rebekah Garris <rebekahlgarris@gmail.com>
2024-05-20 11:47:42 -0500

    adding validate endpoint to allowed sites in security config

--- OneThingApp/src/main/java/org/launchcode/OneThingApp/config/SecurityConfig.java
+++ OneThingApp/src/main/java/org/launchcode/OneThingApp/config/SecurityConfig.java
@@ -45,7 +45,7 @@ public class SecurityConfig {
 		return http
 				.csrf(AbstractHttpConfigurer::disable)
 				.authorizeHttpRequests(
-						req->req.requestMatchers("/login/**", "/register/**")
+						req->req.requestMatchers("/login/**", "/register/**", "/validate/**")
 						.permitAll()
 						.requestMatchers("/admin_only/**").hasAnyAuthority("ADMIN")
 						.anyRequest()
