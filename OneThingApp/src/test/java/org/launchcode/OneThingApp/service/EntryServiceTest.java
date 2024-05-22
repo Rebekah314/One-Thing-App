7c4f32fc7e49947e822352ff12f07015bbddf239
Rebekah Garris <rebekahlgarris@gmail.com>
2024-05-22 12:00:01 -0500

    added some reminders in comments for unit test formats

--- OneThingApp/src/test/java/org/launchcode/OneThingApp/service/EntryServiceTest.java
+++ OneThingApp/src/test/java/org/launchcode/OneThingApp/service/EntryServiceTest.java
@@ -4,20 +4,23 @@ import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.jupiter.api.Test;
 
+
 class EntryServiceTest {
+	
+	
 
 	@Test
 	void twoPlusTwoEqualsFour() {
 		EntryService entryService = new EntryService();
-		
-		int result = entryService.simpleCalculator(2, 2);
-		
-		assertEquals(4, result);
-		
+		//asssertEquals(expectedValue, actualValue)
+		assertEquals(4, entryService.simpleCalculator(2, 2));
 	}
 	
 	//Tests should only test one thing
 	//No need for public or private
 	//not need to return anything
+	//assertTrue and assertFalse
+	//assertNull and assertNotNull
+	//assertThrows
 
 }
