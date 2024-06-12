9daf887f0554e8b2f3c0f8e3e35289b98e7a3b1e
Rebekah Garris <rebekahlgarris@gmail.com>
2024-06-12 09:36:30 -0500

    adding message of encouragement for a user who has no entries yet

--- OneThingApp/front-end/src/Dashboard/index.js
+++ OneThingApp/front-end/src/Dashboard/index.js
@@ -57,7 +57,7 @@ const Dashboard = () => {
                 ))}
                 </div>
             ) : (
-                <></>
+                <>The best time to plant a tree is 20 years ago. The second best time is today. Let's get started.</>
             )} 
 
         </div>
